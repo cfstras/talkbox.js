0eade22a4b6c59ce7c58b81a0bcbd56d503bef0b
Claus Strasburger <claus@strasburger.de>
2013-04-15 23:38:53 +0200

    Fixed #9: shorter timeouts, aggressive reconnect Added #2, connecting and reconnecting messages Pinned dependency versions Some client refactoring

--- static/client.js
+++ static/client.js
@@ -1,13 +1,33 @@
 
-var socket = io.connect();
+var socket;
+var connect = function(){
+	socket = io.connect('', {
+		'max reconnection attempts': 15,
+		'sync disconnect on unload': true,
+		'connect timeout': 5000
+	});
+	socket.on('msg', onMsg);
+	socket.on('ren', onRen);
+	socket.on('userlist',onUserList);
+	socket.on('err',onErr);
+	socket.on('userleave',onUserLeave);
+	socket.on('userjoin',onUserJoin);
+	socket.on('reload',onReload);
+	socket.on('connect',onConnect);
+	socket.on('welcome',onWelcome);
+	socket.on('disconnect',onDisconnect);
+	socket.on('reconnecting',onReconnecting);
+	socket.on('reconnect_failed',onReconnectFailed);
+	socket.on('connect_failed',onConnectFailed);
+};
 var userlist = [];
 var myId = "";
 
-socket.on('msg', function (data) {
+function onMsg(data) {
 	console.log(data);
 	addMessage(data);
-});
-socket.on('ren', function (data) {
+};
+function onRen(data) {
 	if(data.id === myId) {
 		localStorage.setItem('name',data.name);
 	}
@@ -16,7 +36,68 @@ socket.on('ren', function (data) {
         $(this).text(data.name).fadeIn(100);
     });
 	find(userlist,data.id).name = data.name;
-});
+};
+function onUserList(data) {
+	//TODO check userlist
+	setUserlist(data);
+};
+function onErr(data) {
+	data.server = true;
+	data.name = 'server';
+	addMessage(data);
+};
+function onUserLeave(user) {
+	console.log('dc: '+user.name);
+	var index = findIndexById(userlist, user.id);
+	if(index != -1) {
+		userlist.splice(index,1);
+	} else {
+		console.info('got userleave: '+user+', but not found in list'+userlist);
+	}
+	setUserlist(userlist)
+};
+function onUserJoin(data) {
+	userlist.push(data);
+	setUserlist(userlist);
+};
+function onReload() {
+	location.reload();
+};
+function onConnect() {
+	socket.emit('auth',{
+		secret: localStorage.getItem('secret'),
+		name: localStorage.getItem('name')
+	});
+	overlayMsg("logging in...");
+};
+function onWelcome(data) {
+	localStorage.setItem('secret',data.secret);
+	localStorage.setItem('name',data.name);
+	myId = data.id;
+	$('#overlay').fadeOut(300);
+	$('#overlaymessage').fadeOut(300);
+};
+function onDisconnect() {
+	overlayMsg("disconnected");
+	addMessage({
+		name: 'error',
+		server: true,
+		text: 'disconnected from server',
+		date: new Date()});
+	$('#userlist .inner').fadeOut(200).empty();
+};
+function onReconnecting() {
+	overlayMsg("reconnecting...");
+};
+function onReconnectFailed() {
+	overlayMsg("gave up reconnect for now");
+	setTimeout(connect,1000*60); // sleep 60 seconds, then reconnect
+};
+function onConnectFailed() {
+	overlayMsg("gave up connect for now");
+	setTimeout(connect,1000*60); // sleep 60 seconds, then reconnect
+};
+
 addMessage = function(data) {
 	var date = new Date(data.date);
 	var d = $('<div class="message'
@@ -36,10 +117,7 @@ addMessage = function(data) {
 	},150);
 	return d;
 };
-socket.on('userlist', function (data) {
-	//TODO check userlist
-	setUserlist(data);
-});
+
 setUserlist = function(data) {
 	$('#userlist .inner').fadeIn(50);
 	userlist = data;
@@ -69,47 +147,15 @@ makeUserEl = function(user) {
 				+ '" style="opacity: 0;">'
 				+ user.name + '</span>');
 }
-socket.on('err', function(data) {
-	data.server = true;
-	data.name = 'server';
-	addMessage(data);
-});
-socket.on('userleave', function(user) {
-	console.log('dc: '+user.name);
-	var index = findIndexById(userlist, user.id);
-	if(index != -1) {
-		userlist.splice(index,1);
-	} else {
-		console.info('userleave: '+user+', not found in list'+userlist);
-	}
-	setUserlist(userlist)
-});
-socket.on('userjoin', function(data) {
-	userlist.push(data);
-	setUserlist(userlist);
-});
-socket.on('reload', function() {
-	location.reload();
-});
-socket.on('connect', function() {
-	socket.emit('auth',{
-		secret: localStorage.getItem('secret'),
-		name: localStorage.getItem('name')
+
+function overlayMsg(message) {
+	$('#overlay').fadeIn(300);
+	$('#overlaymessage #connect').fadeOut(300,function() {
+		$(this).html(message).fadeIn(300);
 	});
-});
-socket.on('welcome', function(data) {
-	localStorage.setItem('secret',data.secret);
-	localStorage.setItem('name',data.name);
-	myId = data.id;
-});
-socket.on('disconnect', function() {
-	addMessage({
-		name: 'error',
-		server: true,
-		text: 'disconnected from server',
-		date: new Date()});
-	$('#userlist .inner').fadeOut(200).empty();
-});
+	$('#overlaymessage').fadeIn(300);
+}
+
 send = function() {
 	var inp = $('#inputbox');
 	var text = inp.val();
@@ -127,6 +173,10 @@ send = function() {
 	//},100);
 };
 $(document).ready(function() {
+	//disable JS warning
+	$('#javascript').remove();
+	overlayMsg("connecting...");
+	connect();
 	$('#inputbox').keydown(function(event) {
 		//console.log('shift: '+event.shiftKey+' ctrl: '+event.ctrlKey+' which: '+event.which);
 		if(event.keyCode === 13
