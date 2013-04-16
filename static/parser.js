5ca0d5067d2185caa27ddd109a90713085d6c43b
morth <morth6666@gmail.com>
2013-04-16 13:00:52 +0200

    added linkifier

---
+++ static/parser.js
@@ -0,0 +1,9 @@
+function parse(msg) {
+	//linkify
+	return replaceURLWithHTMLLinks(msg);
+}
+
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a href='$1'>$1</a>"); 
+}
